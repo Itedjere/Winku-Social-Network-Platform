ad505c56f767220b5122212cb51d5067fb52168e
Itedjere <itedjereg77@gmail.com>
2024-11-19 18:36:37 +0100

    I connected apollo client to the app

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,13 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import App from "./App.jsx";
 import "./assets/css/main.min.css";
 import "./assets/css/style.css";
 import "./assets/css/color.css";
 import "./assets/css/responsive.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4040/graphql",
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>
 );
